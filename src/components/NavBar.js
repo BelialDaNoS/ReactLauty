7c6762f91bb0eb4ab52025e1bee72a11ec3a4474
BelialDaNoS <lautarogazzo1@gmail.com>
2021-12-16 16:27:47 -0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,22 @@
 function NavBar() {
     return (
-        <div className='App'>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
-          <div class="container">
-            <a class="navbar-brand" href="#"><u>Local Genérico</u></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+        <div classnameName='App'>
+        <nav classname="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
+          <div classname="container">
+            <a classname="navbar-brand" href="#"><u>Local Genérico</u></a>
+            <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+              <span classname="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarsExample07">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Remeras</a>
+            <div classname="collapse navbar-collapse" id="navbarsExample07">
+              <ul classname="navbar-nav me-auto mb-2 mb-lg-0">
+                <li classname="nav-item">
+                  <a classname="nav-link active" aria-current="page" href="#">Remeras</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active" href="#">Pantalones</a>
+                <li classname="nav-item">
+                  <a classname="nav-link active" href="#">Pantalones</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled">Calza2</a>
+                <li classname="nav-item">
+                  <a classname="nav-link disabled">Calza2</a>
                 </li>
               </ul>
             </div>
