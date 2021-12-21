49f5888886736f77f9b97b1ab0ca7a8423535ad2
Belial <89362682+BelialDaNoS@users.noreply.github.com>
2021-12-21 17:53:05 -0300

    React que anda xd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import contador from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
-import List from "./components/list";
 
 function App(){
   return( 
-    <List/>
-    <Contador stock={5} initial={1}/>
     <NavBar/>
   );
 }
--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,10 +1,8 @@
 const carro = require("./carro.png");
-import contador from "./ItemListContainer";
 
 const CartWidget = () => {
   return (
     <div>
-        <p>{contador}</p>
         <img src={carro}/>
     </div>
   )
