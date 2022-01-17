fbf1478fc57e1540b8350b6c388fb07cc71cfa56
BelialDaNoS <lautarogazzo1@gmail.com>
2022-01-17 17:58:57 -0300

    sd

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import Cart from "./components/Cart";
+import CartProvider from "./components/CartContext";
 
 function App(){
   return( 
     <div className="App">
-      <BrowserRouter>
-      <NavBar/>
-        <Routes>
-          <Route path='/'element={<ItemListContainer/> } />
-          <Route path='/tipos/:id'element={<ItemListContainer/>}/>
-          <Route path='/item/:id'element={<ItemDetailContainer/>}/>
-          <Route path='/Cart'element={<Cart/>}/>
-        </Routes>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+        <NavBar/>
+          <Routes>
+            <Route path='/'element={<ItemListContainer/> } />
+            <Route path='/tipos/:id'element={<ItemListContainer/>}/>
+            <Route path='/item/:id'element={<ItemDetailContainer/>}/>
+            <Route path='/Cart'element={<Cart/>}/>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </div>
   );
 }
--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
+
 const carro = require("./carro.png");
 
 const CartWidget = ({compra}) => {
 
   return (
-
         <div className="d-flex justify-content-end">
           <h4 className="row text-white align-content-start">{compra}</h4>
-          <img src={carro} className="row"/>
+          <Link to={"/cart"}><img src={carro} className="row"/></Link> 
         </div>
   )
 }
